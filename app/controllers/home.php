856e923ce50d4064e29bc20719c64e90f5e2d970
momchilmitev <momchilmitev87@gmail.com>
2021-10-31 17:37:15 +0100

    added the home class and index method and the logic for calling it

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,1 +1,9 @@
 <?php
+
+class Home
+{
+    public function index()
+    {
+        echo 'This is the index method in the home controller!';
+    }
+}
--- app/core/app.php
+++ app/core/app.php
@@ -10,13 +10,23 @@ class App
     {
         $url = $this->splitURL();
 
-        if (file_exists('../controllers/' . strtolower($url[0]) . '.php')) {
+        if (file_exists('../app/controllers/' . strtolower($url[0]) . '.php')) {
             $this->controller = strtolower($url[0]);
             unset($url[0]);
         }
 
-        require('../controllers/' . strtolower($url[0]) . '.php');
+        require('../app/controllers/' . $this->controller . '.php');
         $this->controller = new $this->controller();
+
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        $this->parameters = array_values($url);
+        call_user_func_array([$this->controller, $this->method], $this->parameters);
     }
 
     public function splitURL()
