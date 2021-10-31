7f7b763bcbf967a9d34a3ff1f9ac9ea33bf88d13
momchilmitev <momchilmitev87@gmail.com>
2021-10-31 09:24:27 +0100

    added the logic for init the controller in the app constructor

--- app/core/app.php
+++ app/core/app.php
@@ -2,11 +2,21 @@
 
 class App
 {
+    protected $controller = 'home';
+    protected $method = 'index';
+    protected $parameters = [];
+
     public function __construct()
     {
         $url = $this->splitURL();
 
-        print_r($url);
+        if (file_exists('../controllers/' . strtolower($url[0]) . '.php')) {
+            $this->controller = strtolower($url[0]);
+            unset($url[0]);
+        }
+
+        require('../controllers/' . strtolower($url[0]) . '.php');
+        $this->controller = new $this->controller();
     }
 
     public function splitURL()
